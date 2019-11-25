08d7e69ce4b81b0847b20762905c9fd1fd575b72
Mr.Eagle <jostinvalencia97@gmail.com>
2019-11-25 00:16:34 -0500

    Added some Assets

---
+++ Assets/_Scripts/PlayerEvents.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerEvents : MonoBehaviour
+{
+    #region Events
+        public static UnityAction OnTouchpadUp = null;
+        public static UnityAction OnTouchpadDown = null;
+        public static UnityAction <OVRInput.Controller, GameObject> OnControllerSource = null;
+    #endregion
+
+    #region Anchors
+        public GameObject m_LeftAnchor;
+        public GameObject m_RightAnchor;
+        public GameObject m_HeadAnchor;
+    #endregion
+
+    #region Input
+        private Dictionary<OVRInput.Controller, GameObject> m_ControllerSets = null;
+        private OVRInput.Controller m_Inputsource = OVRInput.Controller.None;
+        private OVRInput.Controller m_Controller = OVRInput.Controller.None;
+        private bool m_InputActive = true;
+    #endregion  
+
+    private void Awake()
+    {
+        OVRManager.HMDMounted += PlayerFound;
+        OVRManager.HMDUnmounted += PlayerLost;
+
+        m_ControllerSets = CreateControllerSets();
+    }
+
+    private void OnDestroy()
+    {
+        OVRManager.HMDMounted -= PlayerFound;
+        OVRManager.HMDUnmounted -= PlayerLost;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check For Active Input
+        if (!m_InputActive)
+        {
+            return;
+        }
+        //check if controller exist
+        CheckForController();
+        //check For Input Source
+        CheckInputsource();
+
+        //check for actual input
+        Input();
+    }
+
+    private void CheckForController(){
+        
+        OVRInput.Controller controllerCheck = m_Controller;
+
+        //Right Remote
+        if (OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote))
+        {
+            controllerCheck = OVRInput.Controller.RTrackedRemote;
+        }
+        //Left Remote
+        if (OVRInput.IsControllerConnected(OVRInput.Controller.LTrackedRemote))
+        {
+            controllerCheck = OVRInput.Controller.LTrackedRemote;
+        }
+        //Headset
+        if (!OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote) && !OVRInput.IsControllerConnected(OVRInput.Controller.LTrackedRemote))
+        {
+            controllerCheck = OVRInput.Controller.Touchpad;
+        }
+        //Update
+        m_Controller = UpdateSource(controllerCheck, m_Controller);
+    }
+
+    private void CheckInputsource(){
+        //Update
+        m_Inputsource = UpdateSource(OVRInput.GetActiveController(), m_Inputsource);
+    }
+
+    private void Input(){
+        //TouchPad down
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+            if (OnTouchpadDown != null)
+            {
+                OnTouchpadDown();
+            }
+        }
+        //TouchPad up
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryTouchpad))
+        {
+            if (OnTouchpadUp != null)
+            {
+                OnTouchpadUp();
+            }
+        }
+    }
+
+    private OVRInput.Controller UpdateSource(OVRInput.Controller check, OVRInput.Controller previous){
+        //Values are the same, return
+        if (check == previous)
+        {
+            return previous;
+        }
+        //Get controller Object
+        GameObject controllerObject = null;
+        m_ControllerSets.TryGetValue(check, out controllerObject);
+        //If no controller, set to headset
+        if (controllerObject == null)
+        {
+            controllerObject = m_HeadAnchor;
+        }
+        //Send out event
+        if (OnControllerSource != null)
+        {
+            OnControllerSource(check, controllerObject);
+        }
+
+        //Return new value
+        return check;
+    }
+
+    private void PlayerFound(){
+
+    }
+    
+    private void PlayerLost(){
+
+    }
+
+    private Dictionary<OVRInput.Controller, GameObject> CreateControllerSets(){
+        Dictionary<OVRInput.Controller, GameObject> newSets = new Dictionary<OVRInput.Controller, GameObject>(){
+            {OVRInput.Controller.LTrackedRemote, m_LeftAnchor},
+            {OVRInput.Controller.RTrackedRemote, m_RightAnchor},
+            {OVRInput.Controller.Touchpad, m_HeadAnchor},
+        };
+        return newSets;
+    }
+}
---
+++ Assets/_Scripts/Reticle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour
+{
+    public Pointer m_Pointer;
+    public SpriteRenderer m_CircleRenderer;
+
+    public Sprite m_Opensprite;
+    public Sprite m_CloseSprite;
+
+    private Camera m_Camera = null;
+
+    private void Awake()
+    {
+        m_Pointer.OnPointerUpdate += UpdateSprite;
+        m_Camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(m_Camera.gameObject.transform);
+    }
+
+    private void OnDestroy() 
+    {
+        m_Pointer.OnPointerUpdate += UpdateSprite;   
+    }
+
+    private void UpdateSprite(Vector3 point, GameObject hitObject){
+        transform.position = point;
+
+        if(hitObject){
+            m_CircleRenderer.sprite = m_CloseSprite;
+        }else{
+            m_CircleRenderer.sprite = m_Opensprite;
+        }
+    }
+}
